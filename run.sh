d29dbcf57e54718be984ac829d66f400a1792f91
offtechnologies <offtech@offtechnologies.github.io>
2017-12-06 15:37:18 +0000

    update script

--- run.sh
+++ run.sh
@@ -1,16 +1,2 @@
 #!/bin/sh
-
-# generate control channel key if requested
-if [ ! -f /etc/bind/rndc.key ]
-then
-  echo "/etc/bind/rndc.key not found; generating control channel key"
-  rndc-confgen -r /dev/urandom -a -u named
-elif [ ! -f /etc/bind/rndc.key ]
-then
-  echo "/etc/bind/rndc.key not found; control channel not enabled"
-else
-  echo "/etc/bind/rndc.key found; skipping control channel key generation"
-fi
-
-chown -R named:named /var/cache/bind
-/usr/sbin/named -g -u named
+/usr/sbin/named -u named
