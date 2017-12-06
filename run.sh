3046c08e35c4de4ee4b9c27f9e396d7803381dd8
offtechnologies <offtech@offtechnologies.github.io>
2017-12-06 15:59:47 +0000

    update script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 chown -R named:named /var/bind /etc/bind /var/run/named /var/log/named
 chmod -R o-rwx /var/bind /etc/bind /var/run/named /var/log/named
-/usr/sbin/named -u named
+/usr/sbin/named -g -u named
